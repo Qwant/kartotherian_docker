27239098b09e79dd1c5334477131dea7ee1f370f
nlehuby <n.lehuby@qwant.com>
2017-10-31 14:22:22 +0100

    load_db : remove water_name from the tile schema

--- load_db/generated_sql.sql
+++ load_db/generated_sql.sql
@@ -1600,51 +1600,6 @@ CREATE CONSTRAINT TRIGGER trigger_refresh
     FOR EACH ROW
     EXECUTE PROCEDURE water_point.refresh();
 
--- etldoc: layer_water_name[shape=record fillcolor=lightpink, style="rounded,filled",
--- etldoc:     label="layer_water_name | <z0_8> z0_8 | <z9_13> z9_13 | <z14_> z14+" ] ;
-
-CREATE OR REPLACE FUNCTION layer_water_name(bbox geometry, zoom_level integer)
-RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de text, tags hstore, class text) AS $$
-    -- etldoc: osm_water_lakeline ->  layer_water_name:z9_13
-    -- etldoc: osm_water_lakeline ->  layer_water_name:z14_
-    SELECT osm_id, geometry, name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    tags,
-    'lake'::text AS class
-    FROM osm_water_lakeline
-    WHERE geometry && bbox
-      AND ((zoom_level BETWEEN 9 AND 13 AND LineLabel(zoom_level, NULLIF(name, ''), geometry))
-        OR (zoom_level >= 14))
-    -- etldoc: osm_water_point ->  layer_water_name:z9_13
-    -- etldoc: osm_water_point ->  layer_water_name:z14_
-    UNION ALL
-    SELECT osm_id, geometry, name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    tags,
-    'lake'::text AS class
-    FROM osm_water_point
-    WHERE geometry && bbox AND (
-        (zoom_level BETWEEN 9 AND 13 AND area > 70000*2^(20-zoom_level))
-        OR (zoom_level >= 14)
-    )
-    -- etldoc: osm_marine_point ->  layer_water_name:z0_8
-    -- etldoc: osm_marine_point ->  layer_water_name:z9_13
-    -- etldoc: osm_marine_point ->  layer_water_name:z14_
-    UNION ALL
-    SELECT osm_id, geometry, name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    tags,
-    place::text AS class
-    FROM osm_marine_point
-    WHERE geometry && bbox AND (
-        place = 'ocean'
-        OR (zoom_level >= 1 AND zoom_level <= "rank" AND "rank" IS NOT NULL)
-        OR (zoom_level >= 8)
-    );
-$$ LANGUAGE SQL IMMUTABLE;
 DO $$ BEGIN RAISE NOTICE 'Layer transportation_name'; END$$;DROP MATERIALIZED VIEW IF EXISTS osm_transportation_name_network CASCADE;
 DROP MATERIALIZED VIEW IF EXISTS osm_transportation_name_linestring CASCADE;
 DROP MATERIALIZED VIEW IF EXISTS osm_transportation_name_linestring_gen1 CASCADE;
