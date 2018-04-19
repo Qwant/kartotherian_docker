ac3681291c14ac081e6de2b6451991d1e5b5bcda
Adrien Matissart <a.matissart@qwantresearch.com>
2018-04-19 15:02:10 +0200

    Kubernetes configs for initial import to postgres

--- load_db/import_data.sh
+++ load_db/import_data.sh
@@ -12,8 +12,8 @@ echo 'importing osm data in postgres'
 mkdir -p ${MAIN_DIR}/imposm
 
 # if there is a backup schema imposm cannot delete the tables in to (with the -deployproduction, so we delete them to be able to reload the data several times
-psql -Xq -h postgres -U $user -d $database --set ON_ERROR_STOP="1" -c "drop schema if exists backup cascade;"
-psql -Xq -h postgres -U $user -d $database --set ON_ERROR_STOP="1" -c "CREATE TABLE IF NOT EXISTS wd_names (id          varchar(20) UNIQUE, page          varchar(200) UNIQUE,    labels      hstore);"
+psql -Xq -h $host -U $user -d $database --set ON_ERROR_STOP="1" -c "drop schema if exists backup cascade;"
+psql -Xq -h $host -U $user -d $database --set ON_ERROR_STOP="1" -c "CREATE TABLE IF NOT EXISTS wd_names (id          varchar(20) UNIQUE, page          varchar(200) UNIQUE,    labels      hstore);"
 
 cd $MAIN_DIR/config/import_data
 # run the python script that loads all the data
@@ -22,4 +22,6 @@ cd $MAIN_DIR/config/import_data
 INVOKE_DATA_DIR=$DATA_DIR INVOKE_OSM_FILE=$osm_file pipenv run invoke
 
 # we tell redis that the import is finished so tilerator can start
-redis-cli -h redis set 'data_imported' 'true'
+if [ -n "$REDIS_SET_KEY" ]; then
+	redis-cli -h redis set "$REDIS_SET_KEY" 'true'
+fi
