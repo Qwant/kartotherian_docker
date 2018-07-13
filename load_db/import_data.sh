735fef63480453569e8ecd76618adacfee76f094
Antoine D <antoine.desbordes@gmail.com>
2018-07-13 14:30:47 +0200

    cleanup script

--- load_db/import_data.sh
+++ load_db/import_data.sh
@@ -4,10 +4,8 @@ set -x
 
 optional_invoke_args=$1
 
-osm_file='${DATA_DIR}/*.pbf'
-
 # run the python script that loads all the data
-INVOKE_DATA_DIR=$DATA_DIR INVOKE_OSM_FILE=$osm_file invoke $optional_invoke_args
+invoke $optional_invoke_args
 
 # we tell redis that the import is finished so tilerator can start
 if [ "$REDIS_SET_KEY" = "true" ]; then
