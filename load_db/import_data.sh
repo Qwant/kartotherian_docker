8080893a974deee6e5a5a1006fb13f8b5f798909
Antoine D <antoine.desbordes@gmail.com>
2018-07-17 16:18:00 +0200

    better volumes management (#41)
    * cleanup load_db docker
    
    * cleanup script
    
    * separate volumes for finer control
    
    * add a local-compose file
    
    this makes it possible to use a shared volume for local testing purpose
    
    just run something like:
    `docker-compose -f docker-compose.yml -f local-compose.yml run --rm -e
    INVOKE_OSM_FILE=/data/input/bob.osm.pbf load_db`
    
    to import the file in `./data/bob.osm.pbf`
    
    * update readme
    
    * add conf
    
    * cleanup exposed port and move it to local file
    
    * corrections after review
    
    * pin config sha1

--- load_db/import_data.sh
+++ load_db/import_data.sh
@@ -2,22 +2,10 @@
 set -e
 set -x
 
-osm_file='${DATA_DIR}/*.pbf'
+optional_invoke_args=$@
 
-user='gis'
-database='gis'
-host='postgres'
-
-echo 'importing osm data in postgres'
-mkdir -p ${MAIN_DIR}/imposm
-
-# if there is a backup schema imposm cannot delete the tables in to (with the -deployproduction, so we delete them to be able to reload the data several times
-psql -Xq -h $host -U $user -d $database --set ON_ERROR_STOP="1" -c "drop schema if exists backup cascade;"
-psql -Xq -h $host -U $user -d $database --set ON_ERROR_STOP="1" -c "CREATE TABLE IF NOT EXISTS wd_names (id          varchar(20) UNIQUE, page          varchar(200) UNIQUE,    labels      hstore);"
-
-cd $MAIN_DIR/config/import_data
 # run the python script that loads all the data
-INVOKE_DATA_DIR=$DATA_DIR INVOKE_OSM_FILE=$osm_file pipenv run invoke
+invoke -f config.yml $optional_invoke_args
 
 # we tell redis that the import is finished so tilerator can start
 if [ "$REDIS_SET_KEY" = "true" ]; then
