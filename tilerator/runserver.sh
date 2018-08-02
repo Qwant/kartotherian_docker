89dab737e738aeb0e210f82bdd3eeb202f3b1178
Antoine D <antoine.desbordes@gmail.com>
2018-08-02 16:15:05 +0200

    remove redis dependency

--- tilerator/runserver.sh
+++ tilerator/runserver.sh
@@ -20,16 +20,6 @@ function wait_for_cassandra() {(
 
 wait_for_cassandra
 
-# wait for the database to be loaded
-while true; do
-     if [ $(redis-cli -h redis get data_imported) ]; then 
-        echo "database loaded, starting tilerator"
-        break
-    fi
-    echo "waiting for the database to be loaded"
-    sleep 1
-done
-
 /usr/bin/nodejs /opt/tilerator/server.js -c /etc/tilerator/config.yaml
 
 sleep infinity
