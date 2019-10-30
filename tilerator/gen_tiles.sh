b3f47592b9025cb500cc20f4926224a6abd42edf
Adrien Matissart <a.matissart@qwantresearch.com>
2019-10-30 15:54:05 +0100

    Use kartotherian monorepo to build tilerator and kartotherian images

--- tilerator/gen_tiles.sh
+++ tilerator/gen_tiles.sh
@@ -6,7 +6,7 @@
 set -e
 set +x
 
-node /opt/tilerator/scripts/tileshell.js \
+node /opt/kartotherian/packages/tilerator/scripts/tileshell.js \
 	--config /etc/tilerator/config.yaml \
 	--source /etc/tilerator/sources.yaml \
 	-j.zoom 0 -j.fromZoom 0 -j.beforeZoom 11 \
@@ -14,7 +14,7 @@ node /opt/tilerator/scripts/tileshell.js \
 	-j.parts 8 -j.deleteEmpty -j.keepJob \
 	-j.checkZoom "-1"
 
-node /opt/tilerator/scripts/tileshell.js \
+node /opt/kartotherian/packages/tilerator/scripts/tileshell.js \
 	--config /etc/tilerator/config.yaml \
 	--source /etc/tilerator/sources.yaml \
 	-j.zoom 0 -j.fromZoom 0 -j.beforeZoom 11 \
