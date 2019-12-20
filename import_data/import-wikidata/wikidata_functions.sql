46f658a2aba3ec1f24efc0596a46faad73af6c1d
Rémi Dupré <r.dupre@qwant.com>
2019-12-20 15:43:37 +0100

    display_poi_weight: compute max and min views with percentiles

--- import_data/import-wikidata/wikidata_functions.sql
+++ import_data/import-wikidata/wikidata_functions.sql
@@ -1,6 +1,27 @@
+-- Compute a percentile on the set of page view counts for POIs for which a
+-- Wikipedia page was found.
+CREATE OR REPLACE FUNCTION poi_pageviews_percentile(fraction REAL)
+RETURNS INTEGER AS
+$$
+    SELECT PERCENTILE_DISC(fraction) WITHIN GROUP (ORDER BY all_poi.max_views)
+    FROM (
+        SELECT MAX(stats.views) AS max_views
+        FROM (
+                SELECT osm_id, name, tags FROM osm_poi_polygon
+                    UNION ALL
+                SELECT osm_id, name, tags FROM osm_poi_point
+            ) AS poi
+        JOIN wd_sitelinks AS site
+            ON site.id = poi.tags->'wikidata'
+        JOIN wm_stats AS stats
+            ON site.lang = stats.lang AND site.title = stats.title
+        GROUP BY poi.osm_id
+    ) AS all_poi
+$$ LANGUAGE sql IMMUTABLE;
+
 -- Override default `poi_display_weight` function from
 -- https://github.com/QwantResearch/openmaptiles/.
--- 
+--
 -- Primarily, this function relies on the count of page views for the Wikipedia
 -- pages of a POI, if no Wikipedia page is found, this will fallback on the
 -- default implementation.
@@ -16,16 +37,16 @@ RETURNS REAL AS $$
         -- https://github.com/QwantResearch/openmaptiles/blob/master/layers/poi/class.sql
         max_rank  CONSTANT REAL := 1000.;
 
-        -- Upper limit to the number of views of a Wikipedia page, this limit
-        -- should be sufficient for a period of time of about a year.
-        max_views CONSTANT REAL := 1e6;
-
         -- Lower limit to the number of view of a Wikipedia page to consider it
         -- relevant. If a page generates less views than this limit, the output
         -- value will be computed as if this page doesn't exist.
-        min_views CONSTANT REAL := 50.;
+        min_views CONSTANT REAL := poi_pageviews_percentile(0.1);
 
-        views_count real;
+        -- Upper limit to the number of views of a Wikipedia page. Pages with a
+        -- greater number of views will have a weight of 1.
+        max_views CONSTANT REAL := poi_pageviews_percentile(0.999);
+
+        views_count REAL;
     BEGIN
         SELECT INTO views_count
             CASE
@@ -39,8 +60,9 @@ RETURNS REAL AS $$
                     0
             END;
         RETURN CASE
-            WHEN views_count > min_views THEN
-                0.5 * (1 + LOG(LEAST(max_views, views_count)) / LOG(max_views))
+            WHEN views_count > min_views AND max_views - min_views > 1 THEN
+                0.5 + 0.5 * LOG(LEAST(max_views, views_count) - min_views)
+                             / LOG(max_views - min_views)
             WHEN name <> '' THEN
                 0.5 * (
                     1 - poi_class_rank(poi_class(subclass, mapping_key))::real / max_rank
