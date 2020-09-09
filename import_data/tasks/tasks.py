3a0fd571496108af27ec0f6d0f0a1198e5c08ba3
Adrien Matissart <a.matissart@qwantresearch.com>
2020-09-09 19:43:41 +0200

    Perf fix: override osml10n_get_country with an alternative implementation

--- import_data/tasks/tasks.py
+++ import_data/tasks/tasks.py
@@ -247,6 +247,34 @@ def run_sql_script(ctx):
         """,
     )
 
+    # override get_country: the original function provided by mapnik-german-l10n is too slow
+    # See https://github.com/giggls/mapnik-german-l10n/issues/54 for more details;
+    # the issue also mentions an alternative fix, not yet available on the latest release (v2.5.9)
+    _execute_sql(
+        ctx,
+        db=ctx.pg.import_database,
+        sql="""
+            CREATE OR REPLACE FUNCTION osml10n_get_country(feature geometry)
+                RETURNS text
+                LANGUAGE plpgsql
+                STABLE PARALLEL SAFE STRICT
+            AS $function$
+                DECLARE
+                    transformed_feature geometry;
+                    country text;
+                BEGIN
+                    transformed_feature := st_centroid(st_transform(feature,4326));
+                    SELECT country_code INTO country
+                    FROM country_osm_grid
+                    WHERE st_contains(geometry, transformed_feature)
+                    ORDER BY area
+                    LIMIT 1;
+                RETURN country;
+                END;
+            $function$;
+        """,
+    )
+
     # load language-related functions
     _run_sql_script(ctx, "import-sql/language.sql")
 
