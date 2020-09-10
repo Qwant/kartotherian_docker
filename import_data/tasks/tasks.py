0074b0bedf3015b68f74de05580afe655a570213
Adrien Matissart <a.matissart@qwantresearch.com>
2020-09-10 10:46:46 +0200

    Move osml10n fixes to separate sql file

--- import_data/tasks/tasks.py
+++ import_data/tasks/tasks.py
@@ -236,44 +236,8 @@ def load_poi(ctx):
 
 @task
 def run_sql_script(ctx):
-    # disable Kanji transliteration (produces invalid utf8)
-    _execute_sql(
-        ctx,
-        db=ctx.pg.import_database,
-        sql="""
-            CREATE OR REPLACE FUNCTION osml10n_kanji_transcript(text) RETURNS text AS $$
-                SELECT NULL::text
-            $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
-        """,
-    )
-
-    # override get_country: the original function provided by mapnik-german-l10n is too slow
-    # See https://github.com/giggls/mapnik-german-l10n/issues/54 for more details;
-    # the issue also mentions an alternative fix, not yet available on the latest release (v2.5.9)
-    _execute_sql(
-        ctx,
-        db=ctx.pg.import_database,
-        sql="""
-            CREATE OR REPLACE FUNCTION osml10n_get_country(feature geometry)
-                RETURNS text
-                LANGUAGE plpgsql
-                STABLE PARALLEL SAFE STRICT
-            AS $function$
-                DECLARE
-                    transformed_feature geometry;
-                    country text;
-                BEGIN
-                    transformed_feature := st_centroid(st_transform(feature,4326));
-                    SELECT country_code INTO country
-                    FROM country_osm_grid
-                    WHERE st_contains(geometry, transformed_feature)
-                    ORDER BY area
-                    LIMIT 1;
-                RETURN country;
-                END;
-            $function$;
-        """,
-    )
+    # osml10n fixes
+    _run_sql_script(ctx, "sql-overrides/osml10n_overrides.sql")
 
     # load language-related functions
     _run_sql_script(ctx, "import-sql/language.sql")
