294217256ba2507ec565310f34795590dd24d8f0
aureliano sinatra <sinaure@gmail.com>
2021-10-06 18:15:51 +0200

    avoid fail in case of readonly fs (#139)
    * add exit code at the end of job load all
    
    * pipe true in case of fail
    
    * Add comment about why mkdir error can be ignored
    
    * Fix 'osmupdate' on read-only fs with explicit tempfiles directory
    
    * fix update
    
    * fix update
    
    * fix init osm
    
    * fix
    
    * fix osm file
    
    Co-authored-by: Aureliano Sinatra <a.sinatra@qwant.com>
    Co-authored-by: Adrien Matissart <a.matissart@qwantresearch.com>

--- import_data/tasks/tasks.py
+++ import_data/tasks/tasks.py
@@ -165,7 +165,10 @@ def _get_osmupdate_options(ctx, box=None):
         bot_left = box.bottom_left
         top_right = box.top_right
         bbox_filter = f"-b={bot_left.lon},{bot_left.lat},{top_right.lon},{top_right.lat}"
-    return f"-v --day --hour --base-url={ctx.osm_update.replication_url} {bbox_filter}"
+    return (
+        f"-v --day --hour --base-url={ctx.osm_update.replication_url} {bbox_filter}"
+        f" --tempfiles={ctx.update_tiles_dir}/temp"
+    )
 
 
 @task
@@ -756,7 +759,14 @@ def init_osm_update(ctx):
     latest state.txt file before .pbf timestamp
     """
     logging.info("initializing osm update from osm file timestamp:")
-    ctx.run(f"mkdir -p {ctx.update_tiles_dir}")
+
+    # In containers with restricted permissions, this 'mkdir' command may fail even
+    # though the folder already exists. That's why the error is ignored.
+    # In most cases, this folder is the mount point of the volume "update_tiles_data",
+    # but creating 'update_tiles_dir' may be necessary in a different volume configuration.
+    # If this directory cannot be created, thistask will fail anyway on 'write_new_state()'.
+    ctx.run(f"mkdir -p {ctx.update_tiles_dir} || true")
+
     raw_osm_datetime = read_osm_timestamp(ctx, ctx.osm.file)
     write_new_state(ctx, raw_osm_datetime)
 
