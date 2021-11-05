94f8e3600d296f77659e53f33ecf35ebe2c4f042
aureliano sinatra <sinaure@gmail.com>
2021-11-05 10:29:35 +0100

    QMAPS-2322 Allow run loaddb entrypoint without config file (#143)
    Co-authored-by: Aureliano Sinatra <a.sinatra@qwant.com>

--- load_db/import_data.sh
+++ load_db/import_data.sh
@@ -5,4 +5,8 @@ set -x
 optional_invoke_args=$@
 
 # run the python script that loads all the data
-invoke -f $INVOKE_CONFIG_FILE $optional_invoke_args
+if [[ -z "${INVOKE_CONFIG_FILE}" ]]; then
+  invoke $optional_invoke_args
+else
+  invoke -f $INVOKE_CONFIG_FILE $optional_invoke_args
+fi
